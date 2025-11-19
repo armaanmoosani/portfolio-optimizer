eba0fdf60f2e35edc4f337c6391d44410a3e3f8d
Armaan Moosani <armaan.moosani9035@.com>
2025-11-18 18:54:25 -0600

    Add configs

---
+++ app/about/page.js
@@ -0,0 +1,13 @@
+export default function AboutPage() {
+    return (
+        <div className="container mx-auto px-4 py-8">
+            <div className="max-w-2xl mx-auto text-center">
+                <div className="prose prose-invert mx-auto text-slate-400">
+                    <p className="text-lg mb-4">
+                        About
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
