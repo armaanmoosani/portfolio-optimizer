d182b6dd90c0a649993102206be1437289114cbf
Armaan Moosani <armaan.moosani9035@.com>
2025-11-24 14:08:09 -0600

    backend fix

--- api/index.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-# Add the project root to sys.path so we can import backend
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from backend.app.main import app
