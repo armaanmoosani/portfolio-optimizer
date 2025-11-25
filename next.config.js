3fa9e08c4e3464cadad65af94b7fdbda987ac636
Armaan Moosani <armaan.moosani9035@.com>
2025-11-24 20:48:49 -0600

    Vercel build issue fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    swcMinify: false, // Fixes Framer Motion production animation issues
+    swcMinify: false, // Disabled due to compatibility
+    webpack: (config, { isServer }) => {
+        // Exclude @react-pdf/renderer from optimization to prevent terser errors
+        if (!isServer) {
+            config.optimization.minimizer = config.optimization.minimizer.map((minimizer) => {
+                if (minimizer.constructor.name === 'TerserPlugin') {
+                    minimizer.options.exclude = /node_modules\/@react-pdf/;
+                }
+                return minimizer;
+            });
+        }
+        return config;
+    },
     // Rewrites are now handled by app/api/[...path]/route.js
 };
 
