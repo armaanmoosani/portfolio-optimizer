44cf11d69cb0c0e5442db8b7e65c4d6b440825d7
Armaan Moosani <armaan.moosani9035@.com>
2025-12-07 16:39:39 -0600

    revert

--- app/stocks/page.js
+++ app/stocks/page.js
@@ -1,12 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import dynamic from 'next/dynamic';
-
-const StockViewer = dynamic(() => import("@/components/StockViewer"), {
-    ssr: false,
-    loading: () => <div className="min-h-screen flex items-center justify-center"><div className="w-8 h-8 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div></div>
-});
+import StockViewer from "@/components/StockViewer";
 
 export default function StocksPage() {
     return (
