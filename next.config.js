eb0a441a90497b8db69f0770aaedb15f04788838
Armaan Moosani <armaan.moosani9035@.com>
2025-12-13 01:05:48 -0600

    fix: revert basePath configuration on master

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/portfolio-optimizer',
     reactStrictMode: false,
     swcMinify: false, // Disabled due to compatibility
     // Rewrites are now handled by app/api/[...path]/route.js
